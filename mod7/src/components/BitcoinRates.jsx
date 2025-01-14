bfc289054fb06a1c96c401179d6239dbeee13961
SallyBoBally <mesallyv@gmail.com>
2025-01-13 19:06:31 -0800

    lab1

---
+++ mod7/src/components/BitcoinRates.jsx
@@ -0,0 +1,60 @@
+const { useState, useEffect } = require("react");
+
+const currencies = ['USD', 'AUD', 'NZD', 'GBP', 'EUR', 'SGD'];
+
+function BitcoinRates() {
+    const [currancy, setCurrancy] = useState(currencies[0]);
+    const [price, setPrice] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+}
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        const fetchBitcoinPrice = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await fetch(
+                    'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}',
+                    { signal }
+                );
+                if (!response.ok) {
+                    throw new Error('Data error');
+                }
+                const data = await response.json();
+                setPrice(data.bitcoin[currency.toLowerCase()]);
+            }
+            catch (err) {
+            if (err.name !== 'Abort') {
+                setError(err.message);
+            }
+        } 
+            finally {
+                setLoading(false);
+            }
+        };
+        fetchBitcoinPrice();
+        return () => controller.abort();
+    }, [currenecy]);
+
+    const options =currencies.map(curr => (
+        <option value={curr} key={curr}>
+            {curr}
+        </option>
+    ));
+    return (
+        <div className="BitconinRates componentBox">
+            <h1>Bitcoin Rate</h1>
+            <label>
+                Currency:
+                <select value={currency} onChange={e => setCurrency(e.target.value)}>
+                    {options}
+                </select>
+            </label>
+        </div>
+    );
+}
+
+export default BitcoinRates
