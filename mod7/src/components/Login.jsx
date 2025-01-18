4ce73c283ceb2d575eb31c116ca49ee788bae913
SallyBoBally <mesallyv@gmail.com>
2025-01-18 08:51:55 -0800

    removed comments

--- mod7/src/components/Login.jsx
+++ mod7/src/components/Login.jsx
@@ -15,14 +15,13 @@ function LoginForm() {
         setSubmitResult('Password must not match email address');
         } else {
         setSubmitResult('Successful login.');
-        handleUpdateUser({ email: userEmail }); // context function
+        handleUpdateUser({ email: userEmail });
         }
     }
 
-    {/* if the user is already logged in, show msg instead of form */}
     if (currentUser.email) return (
         <><p>Welcome {currentUser.email}!</p>
         <button onClick={() => handleUpdateUser({})}>Log Out</button>
         </>
     );
-    // otherwise render same form as previously, no changes
+}
